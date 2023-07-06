11e310c7c59977456401b3b69e4dee1bbefcb1ac
Justyna Dudar <justyna.dudar@talan.com>
2023-07-06 13:30:14 +0200

    commit

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -4,6 +4,7 @@ import com.booleanuk.helpers.ExerciseBase;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Exercise extends ExerciseBase {
     /*
@@ -47,7 +48,9 @@ public class Exercise extends ExerciseBase {
          The method must return the value associated to the provided key from the HashMap created
          in the createPerson method
      */
-
+    public String getValue(String key) {
+        return createPerson().get(key);
+    }
 
 
     /*
@@ -57,7 +60,9 @@ public class Exercise extends ExerciseBase {
          The method must return a boolean that represents whether the string provided exists as a key
          in the provided HashMap
      */
-
+    public boolean hasKey(HashMap<String, String> map, String s) {
+        return map.containsKey(s);
+    }
 
 
     /*
@@ -67,7 +72,9 @@ public class Exercise extends ExerciseBase {
          The method must use the string provided to return the integer contained in the provided HashMap,
          or -1 if the string provided is not a key in the HashMap
      */
-
+    public int getValueOrDefault(HashMap<String, Integer> map, String s) {
+        return map.getOrDefault(s, -1);
+    }
 
 
     /*
@@ -90,12 +97,14 @@ public class Exercise extends ExerciseBase {
         map.put(96, "nice");
         // Write your code below this comment...
 
-
-
+        ArrayList<String> list = new ArrayList<>();
+        for (Integer item : numbers) {
+            if (map.containsKey(item)) list.add(map.get(item));
+        }
 
         // ...and above this comment
 
         // Change the return statement below to return your actual ArrayList
-        return new ArrayList<String>();
+        return list;
     }
 }
